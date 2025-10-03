3abfb1de3964d7970449b6293854c47f5e055bb2
prakashornold <prakashornold@gmail.com>
2025-10-03 06:58:58 +0530

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -105,12 +105,13 @@ export default function LandingPage() {
         </p>
 
      
-      <a
-        href="/home"
-        className="px-6 py-3 bg-amber-400 text-black font-semibold rounded-full shadow-lg hover:bg-amber-500 transition duration-300 mb-8"
-      >
-        Go to Home
-      </a>
+   <a
+  href="/home"
+  className="px-6 py-3 bg-amber-400 text-black font-semibold rounded-full shadow-lg 
+             hover:bg-amber-500 transition duration-300 mt-4 z-30"
+>
+  Go to Home
+</a>
 
         <div className="flex space-x-2">
           {images.map((_, index) => (
